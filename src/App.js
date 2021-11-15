b4dbe1b811b022d3f5df3bbeac87b86aa2dc0fe9
Azumist <lukasz.szkwarek96@gmail.com>
2021-11-15 01:49:20 +0100

    added basic routing (react-dom-router v6 type)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
 
+// const TestPage = (props) => {
+//   let params = useParams();
+//   let location = useLocation();
+//   let navigate = useNavigate();
+
+//   console.log('params: ', params);
+//   console.log('location: ', location);
+//   console.log('navigate: ', navigate);
+  
+//   return (  
+//     <div>
+//       <h1>TEST PAGE</h1>
+//       <Link to={`/${location.pathname}/details`}>To dynamic</Link>
+//       <button onClick={() => navigate('/')}>Go to home</button>
+//       <button onClick={() => navigate('/hats')}>Go to hats</button>
+//       <button onClick={() => navigate(-1)}>Go back</button>
+//     </div>
+//   );
+// };
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+    <Link to="/">To home</Link>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/shop/hats' element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
